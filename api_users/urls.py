edeae30e03a54dcde378298adef766fe620017a7
1ynchk <saspery@yandex.ru>
2025-04-18 12:54:34 +0300

    profile was added

--- api_users/urls.py
+++ api_users/urls.py
@@ -4,7 +4,9 @@ from .views import (
     check_login,
     registration,
     user_login,
-    user_logout
+    user_logout,
+    get_profile_info,
+    edit_profile_info
 )
 
 urlpatterns = [
@@ -12,4 +14,6 @@ urlpatterns = [
    path('registration/', registration, name='registration'),
    path('login/', user_login, name='login'),
    path('logout/', user_logout, name='logout'),
+   path('profile/get-profile-info/', get_profile_info, name='profile-info'),
+   path('profile/edit-profile-info/', edit_profile_info, name='edit-profile-info')
 ]
